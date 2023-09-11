8bebfbdc515cbccf9fa91dbac886e1967de60f25
Jim <j.overmeen@dutchview.com>
2023-09-11 15:30:09 +0200

    Initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com',
+    XRapidAPIHostLabel: 'X-RapidAPI-Host',
+    XRapidAPIHostValue: 'open-weather13.p.rapidapi.com',
+    XRapidAPIKeyLabel: 'X-RapidAPI-Key',
+    XRapidAPIKeyValue: '5ff069bd1fmshcf29bca29f2dce6p1b9930jsn53908529ca1c'
+}
